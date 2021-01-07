e0554eed029519421a1ddd5fc4f448dad2e7d9fd
chenyang <tech127@yuanfeng.cn>
2021-01-07 18:36:08 +0800

    Signed-off-by: chenyang <tech127@yuanfeng.cn> 1

---
+++ install/data/datalog.php
@@ -0,0 +1,41 @@
+<?php
+ return array (
+  'shop' => 
+  array (
+    'shop_api_key' => 'gWp180wX',
+    'shop_wap_url' => 'https://ms.look56.com/',
+    'shop_api_url' => 'https://shops.look56.com/',
+    'shop_admin_api_url' => 'https://admins.look56.com/',
+    'shop_admin_url' => 'https://admins.look56.com/',
+    'shop_app_id' => 102,
+  ),
+  'ucenter' => 
+  array (
+    'ucenter_api_key' => 'gWp180wX',
+    'ucenter_api_url' => 'https://ucenters.look56.com/',
+    'ucenter_app_id' => 104,
+    'ucenter_admin_url' => 'https://ucenteradmins.look56.com/',
+    'ucenter_admin_api_url' => 'https://ucenteradmins.look56.com/',
+  ),
+  'paycenter' => 
+  array (
+    'paycenter_api_key' => 'gWp180wX',
+    'paycenter_api_url' => 'https://paycenters.look56.com/',
+    'paycenter_app_id' => 105,
+    'paycenter_admin_api_url' => 'https://paycenteradmins.look56.com/',
+    'paycenter_admin_url' => 'https://paycenteradmins.look56.com/',
+    'paycenter_api_name' => 'fff',
+  ),
+  'shopwap' => 
+  array (
+    'ShopUrl' => 'https://shops.look56.com/',
+    'WapSiteUrl' => 'https://ms.look56.com/',
+    'UCenterApiUrl' => 'https://ucenters.look56.com/',
+    'PayCenterWapUrl' => 'https://paycenters.look56.com/',
+    'ImApiUrl' => 'http://*/index.php',
+    'copyright' => 'Copyright&nbsp;&copy;&nbsp;2007-2020 版权所有',
+    'title' => '商城触屏版',
+    'paySiteName' => '网付宝',
+    'openWeixinLoginInWeixin' => false,
+  ),
+);
