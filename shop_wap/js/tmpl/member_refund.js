c5d8634a6b5c8f43e99704dd865f79b8e035510a
chenyang <tech127@yuanfeng.cn>
2021-03-01 09:33:51 +0800

    lp

--- shop_wap/js/tmpl/member_refund.js
+++ shop_wap/js/tmpl/member_refund.js
@@ -3,12 +3,39 @@ $(function () {
     var u = getCookie("id");
     var t = new ncScrollLoad;
     var shop_id_wap = getCookie('SHOP_ID_WAP');
-    t.loadInit({
-        url: ApiUrl + "/index.php?ctl=Buyer_Service_Return&met=index&typ=json&shop_id_wap="+shop_id_wap,
-        getparam: {k: e,u:u},
-        tmplid: "refund-list-tmpl",
-        containerobj: $("#refund-list"),
-        iIntervalId: true,
-        data: {WapSiteUrl: WapSiteUrl}
-    })
+
+    $.ajax({
+            type: "post", 
+            url: ApiUrl + "/index.php?ctl=Buyer_Service_Return&met=index&typ=json&shop_id_wap="+shop_id_wap, 
+            data: {k: e, u:u}, 
+            dataType: "json", 
+            success: function (e)
+            {
+
+                if (e.status == 200) {
+                    var html = template.render('refund-list-tmpl', e.data);
+                    Zepto("#refund-list").html(html);
+                    console.log(e);
+                } else {
+                     Zepto.sDialog({
+                            skin: "red",
+                            content: '请登录！',
+                            okBtn: false,
+                            cancelBtn: false
+                    });
+                }
+                
+
+            }
+        })
+    // t.loadInit({
+
+
+    //     url: ApiUrl + "/index.php?ctl=Buyer_Service_Return&met=index&typ=json&shop_id_wap="+shop_id_wap,
+    //     getparam: {k: e,u:u},
+    //     tmplid: "refund-list-tmpl",
+    //     containerobj: $("#refund-list"),
+    //     iIntervalId: true,
+    //     data: {WapSiteUrl: WapSiteUrl}
+    // })
 });
