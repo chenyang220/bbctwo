c5ed56709aadc6dd2a5df61e45b63d5cc7868483
chenyang <tech127@yuanfeng.cn>
2021-04-16 09:24:34 +0800

    ll

--- shop/shop/controllers/Seller/Service/abs.php
+++ shop/shop/controllers/Seller/Service/abs.php
@@ -8,3 +8,68 @@ Array
         )
 
 )
+Array
+(
+    [cmd_id] => -140
+    [status] => 250
+    [msg] => failure777
+    [data] => Array
+        (
+            [status] => 250
+        )
+
+)
+Array
+(
+    [cmd_id] => -140
+    [status] => 250
+    [msg] => failure777
+    [data] => Array
+        (
+            [status] => 250
+        )
+
+)
+Array
+(
+    [cmd_id] => -140
+    [status] => 250
+    [msg] => failure777
+    [data] => Array
+        (
+            [status] => 250
+        )
+
+)
+Array
+(
+    [cmd_id] => -140
+    [status] => 250
+    [msg] => failure777
+    [data] => Array
+        (
+            [status] => 250
+        )
+
+)
+Array
+(
+    [cmd_id] => -140
+    [status] => 250
+    [msg] => failure777
+    [data] => Array
+        (
+            [status] => 250
+        )
+
+)
+Array
+(
+    [cmd_id] => -140
+    [status] => 200
+    [msg] => success
+    [data] => Array
+        (
+        )
+
+)
